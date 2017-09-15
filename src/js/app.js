5780816d93cd6248f3df638af272017bc1d54660
Alexandre Silva <contato@silvalexandre.com>
2017-09-15 11:37:35 -0300

    adds audio support; minor improvements

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 import angular from 'angular';
 angular.module('musicApp', []).controller('musicCtrl', musicCtrl);
+let position;
+let songFile;
 
 function musicCtrl() {
 	const vm = this;
@@ -15,17 +17,56 @@ function musicCtrl() {
 	vm.actions = {
 		play: false,
 		vol: 10,
+		hasFile: false,
 		song: {}
 	};
-	vm.change = () => {
+	vm.change = (inputFile) => {
 		console.log('file changed');
+		if (!inputFile.files.length) return false;
+		const reader = new FileReader();
+		const file = inputFile.files[0];
+		// TODO: change actions.hasFile to true, actions.play to false;
+		clearInterval(position);
+		clearTime();
+		vm.actions.play = false;
+		document.getElementById('play').pause();
+		reader.onload = e => {
+			vm.actions.hasFile = true;
+			songFile = file.name.split('.')[0];
+			document.getElementById('play').setAttribute('src', e.target.result);
+		};
+		reader.onerror = () => {
+			vm.actions.hasFile = false;
+			alert('Song Reader play error');
+			console.log(reader.error);
+		};
+		reader.readAsDataURL(file);
 	};
+	vm.playPause = () => {
+		if (vm.actions.play) {
+			document.getElementById('play').pause();
+			clearInterval(position);
+		} else {
+			document.getElementById('play').play();
+			position = setInterval(currentTime, 1000);
+		}
+		vm.actions.play = !vm.actions.play;
+	}
 	vm.volume = up => {
 		if (!up && vm.actions.vol > 0) {
 			vm.actions.vol -= 1;
 		} else if (up && vm.actions.vol < 10) {
 			vm.actions.vol += 1;
 		}
+		document.getElementById('play').volume = (vm.actions.vol > 0 ? vm.actions.vol / 10 : 0);
 	}
+}
+
+function currentTime() {
+	console.log('currentTime');
+}
+
+function clearTime() {
+	console.log('clearTime');
 }
 musicCtrl.$inject = [];
