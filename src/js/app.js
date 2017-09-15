d8a5aa7d3bae9bd733f4c2004fb7d045e45ab5b1
Alexandre Silva <contato@silvalexandre.com>
2017-09-14 21:53:22 -0300

    adds button to change file; layout fixes

--- src/js/app.js
+++
@@ -1,6 +0,0 @@
- function component() {
- 	const element = document.createElement('div');
- 	element.innerHTML = 'Agora com postcss';
- 	return element;
- }
- document.body.appendChild(component());
